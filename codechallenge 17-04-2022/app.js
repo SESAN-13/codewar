d2accff2cbefce316c5cfd417ac8f454e2ff2480
SESAN-13 <olusesanayodele13@gmail.com>
2022-04-17 20:01:06 +0100

    we about to commit

---
+++ codechallenge 17-04-2022/app.js
@@ -0,0 +1,15 @@
+function stringToArray(string){
+    return (string.split(' '))
+  
+      // code code code
+  
+  }
+  stringToArray('Robin Singh')
+
+
+  function double(arr){
+      return arr.map(e=>e*=2)
+
+  }
+  console.log(double([2,4,6,8]))
+  
