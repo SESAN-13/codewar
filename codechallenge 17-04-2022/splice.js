deaa912ee3f9d6d24100c30c5181f3ed2157b9d7
SESS <olusesanayodele13@gmail.com>
2022-06-22 19:49:32 +0100

    splice

--- codechallenge 17-04-2022/splice.js
+++ codechallenge 17-04-2022/splice.js
@@ -7,7 +7,7 @@ function lone(str) {
     let arr = str.split('')
     for (let i = 1; i < arr.length - 1; i++){
         if (arr[i - 1] == 7 && arr[i] == 9 && arr[i + 1] == 7)
-            arr.splice(i,1)
+            arr.splice(i, 1);
     }
     return arr.join('')
 }
