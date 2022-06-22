10e0c8115726359d1fbb101821099f7344695093
SESS <olusesanayodele13@gmail.com>
2022-06-22 19:33:04 +0100

    spliceMethod

---
+++ codechallenge 17-04-2022/splice.js
@@ -0,0 +1,13 @@
+// Write a function that removes every lone 9 that is inbetween 7s.
+
+// "79712312" --> "7712312"
+// "79797"    --> "777"
+
+function lone(str) {
+    let arr = str.split('')
+    for (let i = 1; i < arr.length - 1; i++){
+        if (arr[i - 1] == 7 && arr[i] == 9 && arr[i + 1] == 7)
+            arr.splice(i,1)
+    }
+    return arr.join('')
+}
